e839158be9f7976d2c90e129d38e040c0182731e
ben <eagleowlhawk@gmail.com>
2024-12-27 14:04:18 +0200

    SB-1 Initialize ServerProcCommunicator with reset previous sync states
    Reset named semaphores before use. When Server starts it must be in clean state.
    
    https://github.com/MaksymT17/silber/issues/9

--- ServerProcCommunicator.cpp
+++ ServerProcCommunicator.cpp
@@ -5,8 +5,17 @@ ServerProcCommunicator::ServerProcCommunicator(
 {
     m_sender = std::make_unique<SharedMemorySender>(m_slave_mem_name.c_str());
     m_receiver = std::make_unique<SharedMemoryReceiver>(m_master_mem_name.c_str());
+
 #ifndef _WIN32
 
+    // Unlink semaphores to ensure fresh initialization
+    sem_unlink(m_master_received_s.c_str());
+    sem_unlink(m_slave_received_s.c_str());
+    sem_unlink(m_master_sent_s.c_str());
+    sem_unlink(m_slave_sent_s.c_str());
+    sem_unlink(m_slave_ready_s.c_str());
+
+    // Create semaphores with desired initial values
     m_master_received = sem_open(m_master_received_s.c_str(), O_CREAT, 0666, N_SEM_OFF);
     m_slave_received = sem_open(m_slave_received_s.c_str(), O_CREAT, 0666, N_SEM_OFF);
     m_master_sent = sem_open(m_master_sent_s.c_str(), O_CREAT, 0666, N_SEM_OFF);
@@ -14,30 +23,31 @@ ServerProcCommunicator::ServerProcCommunicator(
     m_slave_ready = sem_open(m_slave_ready_s.c_str(), O_CREAT, 0666, N_SEM_ON);
 
     if (m_master_received == SEM_FAILED || m_slave_received == SEM_FAILED ||
-        m_master_sent == SEM_FAILED || m_slave_sent == SEM_FAILED || m_slave_ready == SEM_FAILED || m_slave_ready == SEM_FAILED)
+        m_master_sent == SEM_FAILED || m_slave_sent == SEM_FAILED || m_slave_ready == SEM_FAILED)
     {
-        std::cerr <<"ProcCommunicator sem_open failure.\n";
-        exit(1);
+        perror("sem_open failure");
+        exit(EXIT_FAILURE);
     }
 
 #else
 
     std::wstring wshMemName(shMemName.begin(), shMemName.end());
 
-    m_master_received = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_m_rsem\n").c_str());
-    m_slave_received = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_s_rsem\n").c_str());
-    m_master_sent = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_m_sent\n").c_str());
-    m_slave_sent = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_s_sent\n").c_str());
-    m_slave_ready = CreateSemaphoreW(NULL, N_SEM_ON, MAXLONG, (wshMemName + L"_s_ready\n").c_str());
+    // Create semaphores with desired initial values
+    m_master_received = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_m_rsem").c_str());
+    m_slave_received = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_s_rsem").c_str());
+    m_master_sent = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_m_sent").c_str());
+    m_slave_sent = CreateSemaphoreW(NULL, N_SEM_OFF, MAXLONG, (wshMemName + L"_s_sent").c_str());
+    m_slave_ready = CreateSemaphoreW(NULL, N_SEM_ON, MAXLONG, (wshMemName + L"_s_ready").c_str());
 
-    if (m_master_received == NULL || m_slave_received == NULL ||
-        m_master_sent == NULL || m_slave_sent == NULL || m_slave_ready == NULL)
+    if (!m_master_received || !m_slave_received || !m_master_sent || !m_slave_sent || !m_slave_ready)
     {
-        std::cerr <<"ProcCommunicator sem_open failure.\n";
-        exit(1);
+        std::cerr << "CreateSemaphore failure, error: " << GetLastError() << '\n';
+        exit(EXIT_FAILURE);
     }
 
 #endif
+
 }
 ServerProcCommunicator::~ServerProcCommunicator()
 {
